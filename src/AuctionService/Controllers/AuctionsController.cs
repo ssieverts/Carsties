e732d8b805f5e49c958596bb5543e34cf78186bf
Steven Sieverts <ssieverts415@gmail.com>
2023-08-20 17:58:55 -0400

    Complete Section 2

---
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AuctionService.Data;
+using AuctionService.Entities;
+using AutoMapper;
+using AuctionService.DTOs;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace AuctionService.Controllers
+{
+    [ApiController]
+    [Route("api/auctions")]
+    public class AuctionsController : ControllerBase
+    {
+        private readonly AuctionDbContext _context;
+        private readonly IMapper _mapper;
+
+        public AuctionsController(AuctionDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        // GET: Auctions
+        [HttpGet]
+        public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+        {
+            var auctions = await _context.Auctions
+              .Include(x => x.Item)
+              .OrderBy(x => x.Item.Make)
+              .ToListAsync();
+
+            return _mapper.Map<List<AuctionDto>>(auctions);
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
+        {
+            var auction = await _context.Auctions
+                .Include(x => x.Item)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (auction == null) return NotFound()
+                    ; 
+
+            return _mapper.Map<AuctionDto>(auction);
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto auctionDto)
+        {
+            var auction = _mapper.Map<Auction>(auctionDto);
+            // TODO: Add user as seller
+            auction.Seller = "test";
+
+            _context.Add(auction);
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if(!result) return BadRequest("Could not save changes to the DB");
+
+            return CreatedAtAction(nameof(GetAuctionById), new { auction.Id }, _mapper.Map<AuctionDto>(auction));
+                       
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
+        {
+            var auction = await _context.Auctions
+                .Include(x => x.Item)
+                .FirstOrDefaultAsync(x => x.Id == id);
+            
+            if (auction == null) return NotFound();
+
+            // TODO: check seller == username
+
+            auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
+            auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
+            auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
+            auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
+            auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if(result) return Ok();
+            
+            return BadRequest("Problem saving changes.");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> UpdateAuction(Guid id)
+        {
+            var auction = await _context.Auctions
+                .Include(x => x.Item)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (auction == null) return NotFound();
+
+            _context.Auctions.Remove(auction);
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (result) return Ok();
+
+            return BadRequest("Problem deleting auction.");
+        }
+
+
+        private bool AuctionExists(Guid id)
+        {
+            return _context.Auctions.Any(e => e.Id == id);
+        }
+    }
+}
--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddDbContext<AuctionDbContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 var app = builder.Build();
